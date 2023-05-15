679fddc600fd47ae48023e048dbba9524f5579d7
Markus <markusderiu610@gmail.com>
2023-05-15 18:24:59 +0200

    Add initial Client implementation
    Selects a random dish from current level at start

---
+++ Assets/ClientsPackage/Scripts/Client.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    private RecipeSO _desiredDish {get; set;}
+    
+    [SerializeField]
+    public LevelSO _currentLevel; //TODO: fetch from Match3 GameObject
+
+    private void Start()
+    {
+        var random = new System.Random();
+        Random.InitState(System.DateTime.Now.Millisecond); //TODO: check if necessary for each client instance
+
+        _desiredDish = _currentLevel.recipes[random.Next(_currentLevel.recipes.Count)];
+
+        Debug.Log(_desiredDish);
+    }
+
+}
