ca172417cc92fceaf4539d44c2531f1d64b2b6f8
Markus <markusderiu610@gmail.com>
2023-05-16 17:21:24 +0200

    Add DiningHall class

--- Assets/ClientsPackage/Scripts/DiningHall.cs
+++ Assets/ClientsPackage/Scripts/DiningHall.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class DiningHall : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private List<Client> _clients;
+
+    [SerializeField] private GameObject _clientPrefab;
+
     void Start()
     {
-        
+        _clients = new List<Client>();
+
+        //TODO: magic numbers
+        transform.GetChild(0).transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 0.2f, Screen.height * 0.8f, 0.0f));
+        transform.GetChild(1).transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 0.5f, Screen.height * 0.8f, 0.0f));
+        transform.GetChild(2).transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 0.8f, Screen.height * 0.8f, 0.0f));
+
+        for(int i = 0; i < 3; i++)
+        {
+            InstantiateClient(new Vector2(transform.GetChild(i).transform.position.x, transform.GetChild(i).transform.position.y));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void InstantiateClient(Vector2 position)
     {
-        
+        GameObject tmp = Instantiate(_clientPrefab, new Vector3(position.x, position.y, 0), Quaternion.identity);
+        _clients.Add(tmp.GetComponent<Client>());
+    }
+
+    void DeleteClient(int idx)
+    {
+        Client tmp = _clients[idx];
+        _clients.RemoveAt(idx);
+        Destroy(tmp.transform.parent);
     }
 }
