b178e1530de0fde4a689d3b15766c77b2d94fa07
Markus <markusderiu610@gmail.com>
2023-05-16 13:07:39 +0200

    Add DesiredDish recipe visualisation
    - DesiredDish shows recipe's ingredients on touch

--- Assets/ClientsPackage/Scripts/DesiredDish.cs
+++ Assets/ClientsPackage/Scripts/DesiredDish.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class DesiredDish : MonoBehaviour
 {
     private Camera _mainCamera;
+    private GameObject _ingredientsList;
 
     [SerializeField]
     private Client _parent;
@@ -15,30 +16,53 @@ public class DesiredDish : MonoBehaviour
 
     private void Awake()
     {
+        //Set camera for RayCasting
         _mainCamera = Camera.main;
     }
 
     private void Start()
     {
+        //Fetch necessary items
         _spriteRenderer.sprite = _parent._desiredDish.Sprite;
+        _ingredientsList = transform.GetChild(0).gameObject;
+
+        //Set ingredients sprites
+        //TODO: ingredients in list(?)
+        if(_parent._desiredDish.Ingredient1 != null)
+        {
+            _ingredientsList.transform.GetChild(1).GetComponent<SpriteRenderer>().sprite = _parent._desiredDish.Ingredient1.Sprite; //TODO: check for exceptions
+        }
+        if(_parent._desiredDish.Ingredient2 != null)
+        {
+            _ingredientsList.transform.GetChild(2).GetComponent<SpriteRenderer>().sprite = _parent._desiredDish.Ingredient2.Sprite; //TODO: check for exceptions
+        }
+        if(_parent._desiredDish.Ingredient3 != null)
+        {
+            _ingredientsList.transform.GetChild(3).GetComponent<SpriteRenderer>().sprite = _parent._desiredDish.Ingredient3.Sprite; //TODO: check for exceptions
+        }
     }
 
-    public void OnTap(InputAction.CallbackContext context) //TODO: one touch handler
+    public void OnTap(InputAction.CallbackContext context) //TODO: one touch handler(?)
     {
         if(!context.started) return;
 
         Vector2 touchPosition;
 
+        //Check platform before retrieving tap/touch position
         #if UNITY_ANDROID || UNITY_IOS
             touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
         #elif UNITY_STANDALONE
             touchPosition = Mouse.current.position.ReadValue();
         #endif
 
+        //Check if tapped/touched on an object
         var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(touchPosition));
         if (!rayHit.collider) return;
         
-        Debug.Log(rayHit.collider.gameObject.name);
+        //Enable/disable ingredients list
+        _ingredientsList.SetActive(!_ingredientsList.activeSelf);
+        _spriteRenderer.enabled = !_spriteRenderer.enabled;
+
     }
     
 }
