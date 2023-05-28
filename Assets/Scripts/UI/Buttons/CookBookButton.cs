1a9f3102537d171e9c279fd5fdbc0abab8c00e7d
Markus <markusderiu610@gmail.com>
2023-05-28 18:12:59 +0200

    Add visuals for Cook Book

---
+++ Assets/Scripts/UI/Buttons/CookBookButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookBookButton : MonoBehaviour
+{
+    [SerializeField] private GameObject _cookBook;
+
+    public void OnTap()
+    {
+        _cookBook.SetActive(true);
+    }
+}
